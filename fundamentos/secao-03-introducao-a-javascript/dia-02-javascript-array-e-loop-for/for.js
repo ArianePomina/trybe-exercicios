3648fe0242633d53be5fae54e01db49062db2934
Ariane Pomina <arianepomina@gmail.com>
2023-03-29 19:35:21 -0300

    for

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/for.js
@@ -0,0 +1,8 @@
+ //utilizar o for para imprimir os elementos da lista groceryList com o console.log()
+
+ let groceryList = ['arroz', 'feijão', 'alface', 'melancia'];
+
+ for (let comidas = 0; comidas < groceryList.length; comidas += 1) {
+    let element = groceryList[comidas];
+    console.log(element);
+ }
