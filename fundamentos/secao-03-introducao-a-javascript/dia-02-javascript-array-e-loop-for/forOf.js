328444165c090e3f695262d38e7a8999dfc20519
Ariane Pomina <arianepomina@gmail.com>
2023-03-29 19:46:04 -0300

    for / of

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/forOf.js
@@ -0,0 +1,7 @@
+//utilizar o for/of para imprimir os elementos da lista names com o console.log()
+
+let names = ['joão', 'maria', 'antonio', 'margarida'];
+
+for (let palavras of names) {
+    console.log(palavras);
+}
