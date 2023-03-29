87024452ed169aeddeda23ae0f4bca4b7376450c
Ariane Pomina <arianepomina@gmail.com>
2023-03-29 18:56:53 -0300

    arrays

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/arrys.js
@@ -0,0 +1,24 @@
+// obter o valor 'serviços' do array menu
+
+let menu = ['home', 'serviços', 'portifolio', 'links'];
+
+let menuServices = menu [1];
+
+console.log(menuServices);
+
+// procurar o índice do valor 'portifolio' do array menu2
+
+let menu2 = ['home', 'serviços', 'portifolio', 'links'];
+
+let indexOfPortifolio = menu2.indexOf('portifolio');
+
+console.log(indexOfPortifolio);
+
+// adicione o valor 'contato' no final do array menu3
+
+let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu3.push('contato');
+
+console.log(menu3);
+
