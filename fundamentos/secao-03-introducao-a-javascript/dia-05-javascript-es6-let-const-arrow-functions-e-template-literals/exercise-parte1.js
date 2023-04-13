8d7b02a4cb2edf0ecd0d5aff6f4fcf66d9b80058
Ariane Pomina <arianepomina@gmail.com>
2023-04-13 20:15:32 -0300

    exercise - prática - parte 2

--- fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/exercise-parte1.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/exercise-parte1.js
@@ -4,3 +4,15 @@ function imprimeIdade() {
   }
 }
 imprimeIdade()
+
+
+  let pessoa = {
+    nome: 'Henri',
+    idade: 20
+  }
+  pessoa = {
+    nome: 'Luna',
+    idade: 19
+  }
+  console.log('Nome:', pessoa.nome);
+  console.log('Idade:', pessoa.idade);
